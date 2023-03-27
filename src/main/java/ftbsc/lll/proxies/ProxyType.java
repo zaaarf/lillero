7a9f01e60f28bd73adfc2ceaf49e092c02a8725f
zaaarf <zaaarf@proton.me>
2023-03-28 00:38:42 +0200

    chore: javadocs

--- src/main/java/ftbsc/lll/proxies/ProxyType.java
+++ src/main/java/ftbsc/lll/proxies/ProxyType.java
@@ -1,11 +1,24 @@
 package ftbsc.lll.proxies;
 
+import ftbsc.lll.proxies.impl.*;
+
 /**
  * An enum listing the various proxies.
  */
 public enum ProxyType {
+
+	/** Indicates it's a {@link FieldProxy}. */
 	FIELD,
+
+	/** Indicates it's a {@link MethodProxy}. */
+
 	METHOD,
+
+	/** Indicates it's a {@link TypeProxy}. */
+
 	TYPE,
+
+	/** Indicates it's a {@link PackageProxy}. */
+
 	PACKAGE
 }
