d02bfda7a53033f50751eedee3567bf85f0c5cc0
zaaarf <me@zaaarf.foo>
2025-01-22 01:45:50 +0100

    chore: cleaned up exceptions

--- src/main/java/ftbsc/lll/exceptions/PatternNotFoundException.java
+++ src/main/java/ftbsc/lll/exceptions/PatternNotFoundException.java
@@ -5,28 +5,10 @@ package ftbsc.lll.exceptions;
  */
 public class PatternNotFoundException extends RuntimeException {
 	/**
-	 * Constructs a new pattern not found exception with the specified detail message.
-	 * @param message the detail message
+	 * Constructs a new pattern not found exception with the given message.
+	 * @param message the given message
 	 */
 	public PatternNotFoundException(String message) {
 		super(message);
-	}
-
-	/**
-	 * Constructs a new pattern not found exception with the specified detail message and cause.
-	 * @param  message the detail message
-	 * @param  cause the cause, may be null (indicating nonexistent or unknown cause)
-	 */
-	public PatternNotFoundException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	/**
-	 * Constructs a new pattern not found exception with the specified cause and a
-	 * detail message of {@code (cause==null ? null : cause.toString())}
-	 * @param  cause the cause, may be null (indicating nonexistent or unknown cause)
-	 */
-	public PatternNotFoundException(Throwable cause) {
-		super(cause);
 	}
 }
